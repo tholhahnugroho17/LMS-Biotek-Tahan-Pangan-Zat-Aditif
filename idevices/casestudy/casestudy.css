786757b03acf20b3460149e36bdceb6b590ddf61
tholhahnugroho17 <tholhahnugroho17@guru.smp.belajar.id>
2026-04-30 11:56:57 +0700

    Add files via upload

---
+++ idevices/casestudy/casestudy.css
@@ -0,0 +1,23 @@
+.caseStudyContent .inline {
+    display: flex;
+}
+
+.caseStudyContent dt {
+    margin: 0 0 0 auto;
+}
+
+.caseStudyContent dd {
+    margin-left: 10px;
+}
+
+.idevice_node.text .iDevice_buttons.feedback-button {
+    margin-bottom: 1em;
+}
+
+.caseStudyContent .CSP-ActivityDiv {
+    margin-bottom: 2.5em;
+}
+
+.caseStudyContent .CSP-Activities .CSP-ActivityDiv:last-child {
+    margin-bottom: 0;
+}
